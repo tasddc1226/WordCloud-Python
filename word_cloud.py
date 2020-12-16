0a2db34bf3817caa1d81b639b1b053476c13e1c8
yang_swim <tasddc@naver.com>
2020-12-17 01:52:47 +0900

    word cloud API와 React app 연동 & 상세 수치 설정

--- word_cloud.py
+++ word_cloud.py
@@ -13,8 +13,16 @@ import matplotlib.pyplot as plt
 # Flask 웹 서버 구축에 필요한 라이브러리
 from flask import Flask, request, jsonify
 
+# CORS를 처리하기 위한 라이브러리
+from flask_cors import CORS
+
+# 특정한 파일이 존재하는지 파일에 접근하기 위한 라이브러리
+import os
+
 # 플라스크 웹 서버 객체 생성
-app = Flask(__name__)
+# 정적 폴더 outputs는 client가 쉽게 접근할 수 있음.
+app = Flask(__name__, static_folder='outputs')
+CORS(app)
 
 # 폰트 경로 설정
 font_path = 'NanumGothic.ttf'
@@ -55,15 +63,15 @@ def make_cloud_image(tags, file_name):
     # 만들어진 이미지 객체를 파일 형태로 저장
     fig.savefig("outputs/{0}.png".format(file_name))
 
-def process_from_text(text, max_count, min_length, words):
-    tags = get_tags(text, max_count, min_length)
+def process_from_text(text, max_count, min_length, words, file_name):
+    tags = get_tags(text, int(max_count), int(min_length))
 
     # 단어 가중치를 적용함.
     for n, c in words.items():
         # 단어가 tag에 포함되어 있다면 가중치를 곱함.
         if n in tags:
-            tags[n] = tags[n] * int(words[n])
-    make_cloud_image(tags, "output")
+            tags[n] = tags[n] * float(words[n])
+    make_cloud_image(tags, file_name)
 
 @app.route("/process", methods=['GET', 'POST'])
 def process():
@@ -72,10 +80,27 @@ def process():
     if content['words'] is not None:
         for data in content['words'].values():
             words[data['word']] = data['weight']
-    process_from_text(content['text'], content['maxCount'], content['minLength'], words)
+    process_from_text(content['text'], content['maxCount'], content['minLength'], words, content['textID'])
     result = {'result':True}
     return jsonify(result)
 
+@app.route('/outputs', methods=['GET', 'POST'])
+def output():
+    text_id = request.args.get('textID')
+    return app.send_static_file(text_id + '.png')
+
+@app.route('/validate', methods=['GET', 'POST'])
+def validate():
+    text_id = request.args.get('textID')
+    path = "outputs/{0}.png".format(text_id)
+    result = {}
+    # 해당 이미지 파일이 존재하는지 확인
+    if os.path.isfile(path):
+        result['result'] = True
+    else:
+        result['result'] = False
+    return jsonify(result)
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000)
+    app.run('0.0.0.0', port=5000, threaded=True)
 
